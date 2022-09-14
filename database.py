69badc0b7ac64ff415aa40a9cf4c2a86643f4a4e
accessibleStats <95653385+accessibleStats@users.noreply.github.com>
2022-09-14 09:56:22 -0500

    Update Comments

--- database.py
+++ database.py
@@ -5,18 +5,14 @@ Script to generate database instance and create users table
 
 """
 
-
+# sqlite3 is a built-in module, no need to install
 import sqlite3
 
-
+# connect command creates and connects to database
 con = sqlite3.connect('user_database.db')
 
+# cursor object allows us to execute SQL commands
 cur = con.cursor()
 
+# execute SQL command to create table
 cur.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, date DATE, username TEXT, email TEXT, passworda TEXT, passwordb TEXT)")
-
-
-
-
-
-
